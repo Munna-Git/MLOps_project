250d2797f08b5515a3df713f862b3380032f439c
Munna-Git <munna88mn@gmail.com>
2025-09-29 12:35:20 +0530

    Adding srcsrc/data/clean_data.py and src/data/clean_data.pytrain_test_split

---
+++ src/data/train_test_split.py
@@ -0,0 +1,85 @@
+import os
+import pandas as pd
+import pickle
+from sklearn.model_selection import train_test_split
+
+# Suppress warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+def load_data(filepath: str) -> pd.DataFrame:
+    """Load the dataset from the specified file path."""
+    return pd.read_csv(filepath)
+
+
+def split_data(df: pd.DataFrame, target: str, test_size: float = 0.1, random_state: int = 42) -> tuple:
+    """
+    Splits the dataset into training and testing sets with stratification on the target variable.
+
+    Parameters:
+    - df (pd.DataFrame): The input dataframe.
+    - target (str): The name of the target column.
+    - test_size (float): The proportion of the data to include in the test split.
+    - random_state (int): Controls the shuffling applied to the data before applying the split.
+
+    Returns:
+    - tuple: X_train, X_test, y_train, y_test
+    """
+    X = df.drop(columns=[target])
+    y = df[target]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, stratify=y, random_state=random_state
+    )
+    return X_train, X_test, y_train, y_test
+
+
+
+def save_data(X: pd.DataFrame, y: pd.Series, data_type: str, stage: str, base_directory: str = '../data'):
+    """
+    Saves features and target variables to the specified stage in both CSV and Pickle format.
+
+    Parameters:
+    - X (pd.DataFrame): Features to save.
+    - y (pd.Series): Target variable to save.
+    - data_type (str): The type of data (e.g., 'train', 'test').
+    - stage (str): The processing stage (e.g., 'processed').
+    - base_directory (str): Base directory for saving data.
+    """
+    directory = os.path.join(base_directory, stage)
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save as Pickle
+    with open(f'{directory}/X_{data_type}.pkl', 'wb') as f:
+        pickle.dump(X, f)
+    with open(f'{directory}/y_{data_type}.pkl', 'wb') as f:
+        pickle.dump(y, f)
+
+
+
+def main():
+    input_filepath = "../data/cleaned/CustomerChurnCleaned.csv"
+    target_column = "Exited"
+    
+
+    #Load_data
+    print("Loading the data....")
+    df = load_data(input_filepath)
+
+    #Split data
+    print("Splitting data into train and test sets...")
+    X_train, X_test, y_train, y_test = split_data(df, target_column)
+
+    #Save data
+    print("Saving split data...")
+    save_data(X_train, y_train, 'train', 'processed')
+    save_data(X_test, y_test, 'test', 'processed')
+
+    print("Train-test split completed. Data saved in the '../data/processed' directory.")
+
+
+if __name__ == "__main__":
+    main()
