0c940f815bb1411025ea5ea2e8c10e0e573ed24e
Munna-Git <munna88mn@gmail.com>
2025-10-04 10:13:21 +0530

    Updated files

--- tests/feature_pipeline_test.py
+++ tests/feature_pipeline_test.py
@@ -1,6 +1,10 @@
 
 import unittest
 from src.data.clean_data import preprocess
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 class TestPreprocessData(unittest.TestCase):
 
